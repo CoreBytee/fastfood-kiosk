5c5279cc876a636b929bdf606348700151a9f660
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-05-15 11:41:02 +0200

    add space key

--- src/assets/script/keyboard.js
+++ src/assets/script/keyboard.js
@@ -2,7 +2,7 @@ const Keys = [
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', "backspace"],
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-    ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm', 'space'],
 ]
 
 const Keyboards = Array.from(
@@ -32,6 +32,8 @@ Keyboards.forEach(
                             () => {
                                 if (Key == "⌫") {
                                     Input.value = Input.value.slice(0, -1)
+                                } else if (Key == "space") {
+                                    Input.value += " "
                                 } else {
                                     Input.value += Key
                                 }
