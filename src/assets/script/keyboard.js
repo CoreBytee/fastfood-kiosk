99f281829dea10c4520cc8b58ac43a452cd36452
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-05-15 11:35:45 +0200

    Create keyboard.js

---
+++ src/assets/script/keyboard.js
@@ -0,0 +1,45 @@
+const Keys = [
+    ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', "backspace"],
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+]
+
+const Keyboards = Array.from(
+    document.querySelectorAll(".keyboard")
+)
+
+Keyboards.forEach(
+    (Keyboard) => {
+        const Input = document.getElementById(Keyboard.getAttribute("inputid"))
+        Keys.forEach(
+            (Row) => {
+                const RowElement = document.createElement("div")
+                RowElement.classList.add("row")
+                Keyboard.appendChild(RowElement)
+                Row.forEach(
+                    (Key) => {
+                        const KeyElement = document.createElement("button")
+                        KeyElement.classList.add("key")
+                        if (Key == "backspace") {
+                            Key = "⌫"
+                        }
+                        KeyElement.textContent = Key
+                        RowElement.appendChild(KeyElement)
+
+                        KeyElement.addEventListener(
+                            "click",
+                            () => {
+                                if (Key == "⌫") {
+                                    Input.value = Input.value.slice(0, -1)
+                                } else {
+                                    Input.value += Key
+                                }
+                            }
+                        )
+                    }
+                )
+            }
+        )
+    }
+)
